3bd7edb6282c1c262fae5f7d6df7ebdc4bbfc004
veggiedude <veggiedude@gmail.com>
2015-09-17 07:44:21 -0700

    reconfigured aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,9 +82,9 @@ Rails.application.configure do
   config.paperclip_defaults={
   	:storage => :s3,
   	:s3_credentials => {
-  	:bucket => ENV['digitaletchings-pb'],
-  	:access_key_id => ENV['AKIAIZ2K6CPWMFS6VNOA'],
-  	:secret_access_key => ENV['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']
+  	:bucket => ENV['AWS_BUCKET'],
+  	:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+  	:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   	}
   	}
   	end
