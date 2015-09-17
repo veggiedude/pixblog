1c00e644e3326d9f66e62218f1d4ef56ab13dab9
veggiedude <veggiedude@gmail.com>
2015-09-16 21:57:54 -0700

    typo fixed on application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
   
   def configure_permitted_parameters
-  	device_parameter_sanitizer.for(:sign_up)<<:name
-  	device_parameter_sanitizer.for(:account_update)<<:name
+  	devise_parameter_sanitizer.for(:sign_up)<<:name
+  	devise_parameter_sanitizer.for(:account_update)<<:name
   	end
 end
