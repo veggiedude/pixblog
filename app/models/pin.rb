6203a2f8d5841375b931c88e21c60ac70e36cd37
veggiedude <veggiedude@gmail.com>
2015-10-10 16:42:02 -0700

    altered app/models/pin.rb to fix heroku image upload issue

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,9 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :image, :styles => {:medium => "300x300>", :thumb => "100x100>"}
+	has_attached_file :image, :styles => {:medium => "300x300>", :thumb => "100x100>"}, :storage => :s3,
+    :s3_credentials => "#{Rails.root}/config/aws.yml",
+    :bucket => "digitaletchings-pb"
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
+
+
